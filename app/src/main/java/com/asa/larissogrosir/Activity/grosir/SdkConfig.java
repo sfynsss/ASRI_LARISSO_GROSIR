c0467a151870e5aac87ad8fc2ebde1998fb78103
sfynsss <sofyansss378@gmail.com>
2021-02-27 08:40:53 +0700

    Commit pertama

--- app/src/main/java/com/asa/larissogrosir/Activity/grosir/SdkConfig.java
+++ app/src/main/java/com/asa/larissogrosir/Activity/grosir/SdkConfig.java
@@ -2,8 +2,8 @@ package com.asa.larissogrosir.Activity.grosir;
 
 public class SdkConfig {
 
-    public static final String MERCHANT_BASE_CHECKOUT_URL = "https://asarasa.id/checkout.php/";
+    public static final String MERCHANT_BASE_CHECKOUT_URL = "https://larisso.co.id/checkout1.php/";
 //    public static final String MERCHANT_BASE_CHECKOUT_URL = "https://192.168.1.15:8000/coba/";
-    public static final String MERCHANT_CLIENT_KEY = "SB-Mid-client-D8pY_4poADw3T9SL";
+    public static final String MERCHANT_CLIENT_KEY = "SB-Mid-client-5ybRjah4KcDwTK_c";
 
 }
